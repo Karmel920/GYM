04c27789605a21a500ce22e53935db9371b0a3db
Karmel920 <mikolajpe2@gmail.com>
2022-12-30 23:22:11 +0100

    meal_plan with calendar

--- public/views/meal_plan.php
+++ public/views/meal_plan.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
 <head>
     <link rel="stylesheet" type="text/css" href="public/css/style_menu.css">
+    <link rel="stylesheet" type="text/css" href="public/css/meal_plan.css">
     <script src="https://kit.fontawesome.com/a0e770b090.js" crossorigin="anonymous"></script>
     <title>MEAL PLAN</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200">
 </head>
 <body>
     <div class="base-container">
@@ -40,9 +42,28 @@
                 <div>Meal plan</div>
             </header>
             <section class='calendar'>
-                <div></div>
+                <div class="wrapper">
+                    <p class="current-date">December 29</p>
+                    <div class="icons">
+                        <span id="prev" class="material-symbols-rounded">chevron_left</span>
+                        <span id="next" class="material-symbols-rounded">chevron_right</span>
+                    </div>
+                    <div class="cal">
+                        <div class="weeks">
+                            <span>Sun</span>
+                            <span>Mon</span>
+                            <span>Tue</span>
+                            <span>Wed</span>
+                            <span>Thu</span>
+                            <span>Fri</span>
+                            <span>Sat</span>
+                        </div>
+                        <div class="days"></div>
+                    </div>
+                </div>
             </section>
         </main>
     </div>
     <script src="public/js/drop_down_menu.js"></script>
+    <script src="public/js/calendar.js"></script>
 </body>
