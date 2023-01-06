b1df6061e75d2d500e7390907213c352895c3fd3
Karmel920 <mikolajpe2@gmail.com>
2023-01-06 11:34:02 +0100

    Connect to database

---
+++ Database.php
@@ -0,0 +1,37 @@
+<?php
+
+// require_once "config.php";
+
+class Database {
+    private $username;
+    private $password;
+    private $host;
+    private $database;
+
+    public function __construct()
+    {
+        $this->username = 'postgres';
+        $this->password = 'postgres';
+        $this->host = '192.168.1.140';
+        $this->database = 'postgres';
+    }
+
+    public function connect()
+    {
+        try {
+            $conn = new PDO(
+                "pgsql:host=$this->host;port=5433;dbname=$this->database",
+                $this->username,
+                $this->password,
+                ["sslmode"  => "prefer"]
+            );
+
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $conn;
+        }
+        catch(PDOException $e) {
+            die("Connection failed: " . $e->getMessage());
+        }
+    }
+}
