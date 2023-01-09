f0c388d5a170bb753c4e9e6b39b00fa0a5bcd1e3
Karmel920 <mikolajpe2@gmail.com>
2023-01-09 19:58:01 +0100

    Remove fields name and surname from `User.php`

--- src/models/User.php
+++ src/models/User.php
@@ -5,15 +5,11 @@ class User
 {
     private $email;
     private $password;
-    private $name;
-    private $surname;
 
-    public function __construct(string $email, string $password, string $name, string $surname)
+    public function __construct(string $email, string $password)
     {
         $this->email = $email;
         $this->password = $password;
-        $this->name = $name;
-        $this->surname = $surname;
     }
 
 
@@ -36,25 +32,4 @@ class User
     {
         $this->password = $password;
     }
-
-    public function getName(): string
-    {
-        return $this->name;
-    }
-
-    public function setName(string $name)
-    {
-        $this->name = $name;
-    }
-
-    public function getSurname(): string
-    {
-        return $this->surname;
-    }
-
-    public function setSurname(string $surname)
-    {
-        $this->surname = $surname;
-    }
-
 }
