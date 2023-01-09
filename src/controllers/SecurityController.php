afac64ca1b6f3203cf022438c96278196ef013a2
Karmel920 <mikolajpe2@gmail.com>
2023-01-09 20:04:59 +0100

    Register user

--- src/controllers/SecurityController.php
+++ src/controllers/SecurityController.php
@@ -2,19 +2,26 @@
 
 require_once 'AppController.php';
 require_once __DIR__.'/../models/User.php';
+require_once __DIR__.'/../repository/UserRepository.php';
 
 class SecurityController extends AppController
 {
     public function login()
     {
-        $user = new User('jsnow@pk.edu.pl', 'admin', 'John', 'Snow');
+        $userRepository = new UserRepository();
 
-//        if($this->isPost()) {
-//            return $this->login('login');
-//        }
+        if(!$this->isPost()) {
+            return $this->render('login');
+        }
 
         $email = $_POST["email"];
-        $password = $_POST["password"];
+        $password = sha1($_POST["password"]);
+
+        $user = $userRepository->getUser($email);
+
+        if(!$user) {
+            return $this->render('login', ['messages' => ['User not exist!']]);
+        }
 
         if ($user->getEmail() !== $email) {
             return $this->render('login', ['messages' => ['User with this email not exist!']]);
@@ -28,6 +35,30 @@ class SecurityController extends AppController
 
         $url = "http://$_SERVER[HTTP_HOST]";
         header("Location: {$url}/meal_plan");
+    }
+
+    public function register()
+    {
+        $userRepository = new UserRepository();
+
+        if(!$this->isPost()) {
+            return $this->render('register');
+        }
+
+        $email = $_POST["email"];
+        $password = sha1($_POST["password"]);
+
+        $user = $userRepository->getUser($email);
+
+        if($user) {
+            return $this->render('register', ['messages' => ['User with this email exist!']]);
+        }
+
+        $userRepository->addNewUser($email, $password);
+        return $this->render('register', ['messages' => ['Registration was successful, sign in!']]);
+
+//        $url = "http://$_SERVER[HTTP_HOST]";
+//        header("Location: {$url}/login");
     }
 
 }
