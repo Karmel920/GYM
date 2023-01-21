84c92d95051c2c67e7fbfcc8d3f9b9daf820025e
Karmel920 <mikolajpe2@gmail.com>
2023-01-21 01:33:51 +0100

    Moved nav from views to `nav.php`

--- public/views/recipes.php
+++ public/views/recipes.php
@@ -11,34 +11,9 @@
 </head>
 <body>
     <div class="base-container">
-        <div class="open-nav"><i class="fa-solid fa-bars"></i></div>
-        <nav class="nav hidden">
-            <div class="close-nav"><i class="fa-solid fa-xmark"></i></div>
-            <img src="public/img/logo_words.svg">
-            <ul>
-                <li>
-                    <i class="fa fa-regular fa-calendar"></i>
-                    <a href="meal_plan" class="button button-meal-plan">Meal plan</a>
-                </li>
-                <li>
-                    <i class="fa fa-solid fa-utensils"></i>
-                    <a href="recipes" class="button button-recipes">Recipes</a>
-                </li>
-                <li>
-                    <i class="fa fa-solid fa-receipt"></i>
-                    <a href="menus" class="button button-menus">Menus</a>
-                </li>
-                <li>
-                    <i class="fa fa-solid fa-dumbbell"></i>
-                    <a href="#" class="button button-trainings">Trainings</a>
-                </li>
-                <li>
-                    <i class="fa fa-solid fa-gear"></i>
-                    <a href="settings" class="button button-settings">Settings</a>
-                    <a href="logout" class="logout"><i class="fa-solid fa-right-from-bracket"></i></a>
-                </li>
-            </ul>
-        </nav>
+        <?php
+        include('nav.php');
+        ?>
         <main>
             <div class="overlay">
                 <div class="popup">
